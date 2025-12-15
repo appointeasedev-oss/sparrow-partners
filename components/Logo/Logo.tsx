83a9a884800e0f61f2336b05052e7badcd1110d4
appointease.dev <appointease.dev@gmail.com>
2025-12-15 17:37:38 +0000

    cvff

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,12 +1,10 @@
 import clsx from "clsx";
-import Image from "next/image";
 import { ComponentProps } from "react";
 import styles from "./Logo.module.css";
 
 export function Logo({ className, ...props }: ComponentProps<"div">) {
   return (
     <div className={clsx(className, styles.logo)} {...props}>
-      <Image src="/logo.png" alt="Sparrow Partners" width={24} height={24} />
     </div>
   );
 }

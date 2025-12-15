5a07a557306a7fd5645ba041cc91e9a59042aafc
appointease.dev <appointease.dev@gmail.com>
2025-12-15 17:40:53 +0000

    cvbnm

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -3,8 +3,5 @@ import { ComponentProps } from "react";
 import styles from "./Logo.module.css";
 
 export function Logo({ className, ...props }: ComponentProps<"div">) {
-  return (
-    <div className={clsx(className, styles.logo)} {...props}>
-    </div>
-  );
+  return <div className={clsx(className, styles.logo)} {...props} />;
 }
